c18debf1231a85255a8b09f8d4863222103a3fdb
Tristen <79072811+tristenCodes@users.noreply.github.com>
2025-08-06 07:13:24 -0500

    Fix broken home screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,16 +2,8 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function Tab() {
   return (
-    <View style={styles.container}>
+    <View >
       <Text>settings</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
